ef9aad2bb3ecaa903f4972d55877c9fde987c0ba
Enki Layman <layman212@gmail.com>
2025-02-28 02:21:46 -0500

    Learned more about string literals

---
+++ String_literals.py
@@ -0,0 +1,12 @@
+# You can escape so that python interprets the next character as a part of the string using the \
+print('You\'re so right, aren\'t you?')
+
+# Multi-line strings:
+print("""
+This 
+is 
+a 
+multi
+line
+string.
+""")
